123fd17be4bd2fea82c8cde670069b9b979bf18f
john Constable <john@metadata.school>
2023-03-26 14:37:56 +0100

    reformat script based on black auto formatter.
    Took the pylint score from 0/10 to 6.8!

--- MergeDungeonPages.py
+++ MergeDungeonPages.py
@@ -8,57 +8,90 @@ This is a script to take the pdf of the excellent print and play game Dungeon Pa
 from PIL import Image
 import pypdfium2 as pdfium
 
-#you need to have libraries Pillow and Pypdfium2 installed. This is tested with Python 3.9.1 , pypdfium2 4.1.0, and pillow something or other
+# you need to have libraries Pillow and Pypdfium2 installed. This is tested with Python 3.9.1 , pypdfium2 4.1.0, and pillow something or other
 
-#importing pdf - put your version of the game here. Your folder should have this script and the game pdf and nothing else because I'm doing everything in place because I don't really understand file handling very well. If you have *both* the core set and the yearlong set then I'd recommend amalgamating the pdfs before you start or amending this script.
+# importing pdf - put your version of the game here. Your folder should have this script and the game pdf and nothing else because I'm doing everything in place because I don't really understand file handling very well. If you have *both* the core set and the yearlong set then I'd recommend amalgamating the pdfs before you start or amending this script.
 
-#if your pdf is called something different, change it below
+# if your pdf is called something different, change it below
 pdf = pdfium.PdfDocument("Dungeon Pages For Combining.pdf")
 n_pages = len(pdf)
-pngscale = 3 #this makes it a good sort of size for iPad
-#and saving out as separate characters and dungeons
+pngscale = 3  # this makes it a good sort of size for iPad
+# and saving out as separate characters and dungeons
 for page_number in range(n_pages):
-	page = pdf.get_page(page_number)
-	#making the character and dungeon sections
+    page = pdf.get_page(page_number)
+    # making the character and dungeon sections
 
-	bitmap = page.render (
-		scale = pngscale,
-		crop = (0, 500, 0, 0),
-	)
-	character = bitmap.to_pil()
-	character.save(f"c{page_number+1}.png")
+    bitmap = page.render(
+        scale=pngscale,
+        crop=(0, 500, 0, 0),
+    )
+    character = bitmap.to_pil()
+    character.save(f"c{page_number+1}.png")
 
-	bitmap = page.render(
-		scale = pngscale,
-		crop = (0, 0, 0, 292),
-	)
-	dungeon = bitmap.to_pil()
-	dungeon.save(f"d{page_number+1}.png")
+    bitmap = page.render(
+        scale=pngscale,
+        crop=(0, 0, 0, 292),
+    )
+    dungeon = bitmap.to_pil()
+    dungeon.save(f"d{page_number+1}.png")
 
-#creating dungeon pages by mashing them together
+# creating dungeon pages by mashing them together
+
 
 def merge(im1, im2):
-	w = max(im1.size[0], im2.size[0])
-	h = im1.size[1] + im2.size[1]
-	im = Image.new("RGBA", (w,h), (255,255,255))
-	im.paste(im1, (0,0))
-	im.paste(im2, (0, 290*pngscale))
-	return im
+    w = max(im1.size[0], im2.size[0])
+    h = im1.size[1] + im2.size[1]
+    im = Image.new("RGBA", (w, h), (255, 255, 255))
+    im.paste(im1, (0, 0))
+    im.paste(im2, (0, 290 * pngscale))
+    return im
 
-#name all the characters and dungeons here for cutely named files. This is the core set and the first nine weeks of the yearlong set. You need to make sure the pages of the PDF are in the same order as this or your names will all come out wrong. If you *only* have the year long set, delete the first six names from each list; they're the core items.
 
-characters = ["Zafinn, Wandering Wizard", "Gloria, Avenging Warrior", "Sygrid, Northern Hunter", "Amador, Light Cleric", "Mira, Haunted Ranger", "Flynn, White Glove Knight", "Amador, Steam Cleric", "Sygrid, Vengeful Hunter", "Gloria, Virtuous Warrior", "Drok, Outcast Troll", "Mira, Brush Ranger", "Zafinn, Mischievous Wizard", "Flynn, Red Glove Knight", "Krete & Kreeg, Renowned Bards", "Amador, Woodward Cleric"]
-dungeons = ["Highmount Village", "Hellenburg", "Cliffdrop City", "Thorne Valley", "Reachport", "The Iron Peaks", "Havington", "Hogglebottom", "Whittleberry", "Bordertown", "Abyssal Plane", "Shan Tomb", "Capital City", "Stormshield", "The Hungry Isles"]
+# name all the characters and dungeons here for cutely named files. This is the core set and the first nine weeks of the yearlong set. You need to make sure the pages of the PDF are in the same order as this or your names will all come out wrong. If you *only* have the year long set, delete the first six names from each list; they're the core items.
 
-#make all the pages
+characters = [
+    "Zafinn, Wandering Wizard",
+    "Gloria, Avenging Warrior",
+    "Sygrid, Northern Hunter",
+    "Amador, Light Cleric",
+    "Mira, Haunted Ranger",
+    "Flynn, White Glove Knight",
+    "Amador, Steam Cleric",
+    "Sygrid, Vengeful Hunter",
+    "Gloria, Virtuous Warrior",
+    "Drok, Outcast Troll",
+    "Mira, Brush Ranger",
+    "Zafinn, Mischievous Wizard",
+    "Flynn, Red Glove Knight",
+    "Krete & Kreeg, Renowned Bards",
+    "Amador, Woodward Cleric",
+]
+dungeons = [
+    "Highmount Village",
+    "Hellenburg",
+    "Cliffdrop City",
+    "Thorne Valley",
+    "Reachport",
+    "The Iron Peaks",
+    "Havington",
+    "Hogglebottom",
+    "Whittleberry",
+    "Bordertown",
+    "Abyssal Plane",
+    "Shan Tomb",
+    "Capital City",
+    "Stormshield",
+    "The Hungry Isles",
+]
+
+# make all the pages
 
 for ii in range(n_pages):
-	for jj in range(n_pages):
+    for jj in range(n_pages):
+        im1 = Image.open(f"c{ii+1}.png")
+        im2 = Image.open(f"d{jj+1}.png")
 
-		im1=Image.open(f"c{ii+1}.png")
-		im2=Image.open(f"d{jj+1}.png")
-
-		dungeonpage = merge(im1, im2)
-		character = characters[ii]
-		dungeon = dungeons[jj]
-		dungeonpage.save(f"The Adventures of {character} in {dungeon}.png"  )
+        dungeonpage = merge(im1, im2)
+        character = characters[ii]
+        dungeon = dungeons[jj]
+        dungeonpage.save(f"The Adventures of {character} in {dungeon}.png")
