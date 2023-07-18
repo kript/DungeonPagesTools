5dd95c14bc187bf2c0427515a5cce5b621dfb6ac
john Constable <john@metadata.school>
2023-07-18 22:32:09 +0100

    working module with main() for testing/standalone

--- zip.py
+++ zip.py
@@ -3,48 +3,59 @@
 import zipfile
 import re
 
-adventure = "Year-long Adventure_071423.zip"
+def adventure_zip_file_info(adventurefile):
+    """
+    Get the list of files in an Year Long Adventure zip
+    return a dictionary with the information.
 
-with zipfile.ZipFile(adventure, mode="r") as archive:
-    list = archive.namelist()
-    items = archive.infolist()
+    sample dictionary entry in dungeon:
+    'DungeonPages_YLA_WK_08_KreteKreeg_Stormshield_022323.pdf':
+    {
+        'adventurer': 'KreteKreeg',
+        'adventurers name': 'Krete '
+        'Kreeg',
+        location': 'Stormshield',
+        location name': 'Stormshield'
+    },
+    """
+    with zipfile.ZipFile(adventurefile, mode="r") as archive:
+        list = archive.namelist()
+        #items = archive.infolist()
+    dungeon = {}
+    for file in list:
+        # ignore the zip artefacts of being created on OSX
+        if file.startswith("__MACOSX"):
+            continue
+        # only get the weekly adventure files, ignore the rules and addendum files
+        if "YLA_WK" in file:
+            # strip off the extension, then break up the filename
+            # the naming isn't consistent, so we need to work backwards
+            namearray = file.split(".")[0].split("_")
+            adventurer = namearray[-3]
+            location = namearray[-2]
+            # the filename has Camel Case adventurer names and locations,
+            # so make them human readable
+            # thanks to stack exchange for the regex- https://stackoverflow.com/a/2279177
+            adventurer_readable = re.sub(r"([A-Z])", r" \1", adventurer).lstrip()
+            location_readable =  re.sub(r"([A-Z])", r" \1", location).lstrip()
+            # create a dictionary entry for each file
+            dungeon[file] = {
+                "adventurer": adventurer,
+                "adventurers name": adventurer_readable,
+                "location": location,
+                "location name": location_readable,
+                }
+    return(dungeon)
 
-dungeon = {}
-for file in list:
-    # ignore the zip artefacts of being created on OSX
-    if file.startswith("__MACOSX"):
-        continue
-    # only get the weekly adventure files, ignore the rules and addendum files
-    if "YLA_WK" in file:
-        #realfiles.append(file)
-        # strip off the extension, then break up the filename
-        # the naming isn't consistent, so we need to work backwards
-        namearray = file.split(".")[0].split("_")
-        adventurer = namearray[-3]
-        location = namearray[-2]
-        # the filename has Camel Case adventurer names and locations,
-        # so make them human readable
-        # thanks stack exchange - https://stackoverflow.com/a/2279177
-        adventurer_readable = ' '.join(re.sub(r"([A-Z])", r" \1", adventurer).split())
-        location_readable =  ' '.join(re.sub( r"([A-Z])", r" \1", location).split())
-        # create a dictionary entry for each file
-        dungeon[file] = {
-            "adventurer": adventurer,
-            "adventurers name": adventurer_readable,
-            "location": location,
-            "location name": location_readable,
-            }
+def main():
+    """
+    for future unit tests, the ability to run this module standalone
+    """
+    adventure = "Year-long Adventure_071423.zip"
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+    dungeon = adventure_zip_file_info(adventure)
+    pp.pprint(dungeon)
 
-""" sample dictionary entry in dungeon:
-"DungeonPages_YLA_WK027_FlynnBlackGlove_Meredrin_070623.pdf":
-{
-    "adventurer": "FlynnBlackGlove",
-    "location": "Meredrin"
-}
-"""
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(dungeon)
-
-
+if __name__ == "__main__":
+    main()
