5e067f56f1a84ba91906d1a17a30b8c0c4101d07
John Constable <john@kript.net>
2023-07-26 07:02:25 +0100

    Function library to get information from the zip files (#38)
    * Function library to get information from the zip files
    
    * working module with main() for testing/standalone
    
    * pylint fixes
    
    * black formatting

---
+++ zip.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Module to manage information from the zip files for the Dungeon Pages Year Long Adventure.
+
+"""
+
+import zipfile
+import re
+import pprint
+
+
+def adventure_zip_file_info(adventurefile):
+    """
+    Get the list of files in an Year Long Adventure zip
+    return a dictionary with the information.
+
+    sample dictionary entry in dungeon:
+    'DungeonPages_YLA_WK_08_KreteKreeg_Stormshield_022323.pdf':
+    {
+        'adventurer': 'KreteKreeg',
+        'adventurers name': 'Krete '
+        'Kreeg',
+        location': 'Stormshield',
+        location name': 'Stormshield'
+    },
+    """
+    with zipfile.ZipFile(adventurefile, mode="r") as archive:
+        file_list = archive.namelist()
+        # items = archive.infolist()
+    dungeon = {}
+    for file in file_list:
+        # ignore the zip artefacts of being created on OSX
+        if file.startswith("__MACOSX"):
+            continue
+        # only get the weekly adventure files, ignore the rules and addendum files
+        if "YLA_WK" in file:
+            # strip off the extension, then break up the filename
+            # the naming isn't consistent, so we need to work backwards
+            namearray = file.split(".")[0].split("_")
+            adventurer = namearray[-3]
+            location = namearray[-2]
+            # the filename has Camel Case adventurer names and locations,
+            # so make them human readable
+            # thanks to stack exchange for the regex- https://stackoverflow.com/a/2279177
+            adventurer_readable = re.sub(r"([A-Z])", r" \1", adventurer).lstrip()
+            location_readable = re.sub(r"([A-Z])", r" \1", location).lstrip()
+            # create a dictionary entry for each file
+            dungeon[file] = {
+                "adventurer": adventurer,
+                "adventurers name": adventurer_readable,
+                "location": location,
+                "location name": location_readable,
+            }
+    return dungeon
+
+
+def main():
+    """
+    for future unit tests, the ability to run this module standalone
+    """
+    adventure = "Year-long Adventure_071423.zip"
+    printer = pprint.PrettyPrinter(indent=4)
+    dungeon = adventure_zip_file_info(adventure)
+    printer.pprint(dungeon)
+
+
+if __name__ == "__main__":
+    main()
