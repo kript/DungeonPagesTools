05a157e35720979a7f7f1c8733fdd056bfcb3c2d
John Constable <john@kript.net>
2023-11-14 19:03:08 +0000

    refactor, lost my place (#41)

--- MergeDungeonPages.py
+++ MergeDungeonPages.py
@@ -9,39 +9,12 @@ from PIL import Image
 import pypdfium2 as pdfium
 import os
 
-# you need to have libraries Pillow and Pypdfium2 installed. This is tested with Python 3.9.1 , pypdfium2 4.1.0, and pillow something or other
-
-# importing pdf - put your version of the game here. Your folder should have this script and the game pdf and nothing else because I'm doing everything in place because I don't really understand file handling very well. If you have *both* the core set and the yearlong set then I'd recommend amalgamating the pdfs before you start or amending this script.
-
-# if your pdf is called something different, change it below
-pdf = pdfium.PdfDocument("Dungeon Pages For Combining.pdf")
-n_pages = len(pdf)
 pngscale = 3  # this makes it a good sort of size for iPad
-# and saving out as separate characters and dungeons
-path = 'combined'
-os.mkdir(path)
-for page_number in range(n_pages):
-    page = pdf.get_page(page_number)
-    # making the character and dungeon sections
 
-    bitmap = page.render(
-        scale=pngscale,
-        crop=(0, 500, 0, 0),
-    )
-    character = bitmap.to_pil()
-    character.save(f"combined/c{page_number+1}.png")
-
-    bitmap = page.render(
-        scale=pngscale,
-        crop=(0, 0, 0, 292),
-    )
-    dungeon = bitmap.to_pil()
-    dungeon.save(f"combined/d{page_number+1}.png")
-
-# creating dungeon pages by mashing them together
-
-
-def merge(im1, im2):
+def merge(im1, im2,pngscale):
+    """
+    creating dungeon pages by mashing them together
+    """
     w = max(im1.size[0], im2.size[0])
     h = im1.size[1] + im2.size[1]
     im = Image.new("RGBA", (w, h), (255, 255, 255))
@@ -49,94 +22,143 @@ def merge(im1, im2):
     im.paste(im2, (0, 290 * pngscale))
     return im
 
+def split_pdf(pngscale):
+    """
+    take the combined PDF file and split it into two png file per page
+    """
+    pdf = pdfium.PdfDocument("Dungeon Pages For Combining.pdf")
+    n_pages = len(pdf)
+    # and saving out as separate characters and dungeons
+    path = 'combined'
+    os.mkdir(path)
+    for page_number in range(n_pages):
+        page = pdf.get_page(page_number)
+        # making the character and dungeon sections
 
-# name all the characters and dungeons here for cutely named files. This is the core set and the first nine weeks of the yearlong set. You need to make sure the pages of the PDF are in the same order as this or your names will all come out wrong. If you *only* have the year long set, delete the first six names from each list; they're the core items.
+        bitmap = page.render(
+            scale=pngscale,
+            crop=(0, 500, 0, 0),
+        )
+        character = bitmap.to_pil()
+        character.save(f"combined/c{page_number+1}.png")
 
-characters = [
-    "Zafinn, Wandering Wizard",
-    "Gloria, Avenging Warrior",
-    "Sygrid, Northern Hunter",
-    "Amador, Light Cleric",
-    "Mira, Haunted Ranger",
-    "Flynn, White Glove Knight",
-    "Amador, Steam Cleric",
-    "Sygrid, Vengeful Hunter",
-    "Gloria, Virtuous Warrior",
-    "Drok, Outcast Troll",
-    "Mira, Brush Ranger",
-    "Zafinn, Mischievous Wizard",
-    "Flynn, Red Glove Knight",
-    "Krete & Kreeg, Renowned Bards",
-    "Amador, Woodward Cleric",
-    "Zafinn, Elemental Wizard",
-    "Sygrid, Banished Hunter",
-    "Gloria, Daredevil Warrior",
-    "Valensi, Abyss Whisper",
-    "Mira, Lawless Ranger",
-    "Flynn, Blue Glove Knight",
-    "Drok, Adventurous Troll",
-    "Krete & Kreeg, Questing Bards",
-    "Zafinn, Brash Wizard",
-    "Hardos, Valiant Monk",
-    "Valensi, Spirit Whisper",
-    "Sygrid, Orphaned Hunter",
-    "Amador, Martial Cleric",
-    "Gloria, Night Warrior",
-    "Marian, Devious Spirit",
-    "Mira, Sky Ranger",
-    "Krete And Kreeg, Notorious Bards",
-    "Flynn, Black Glove",
-    "Finley,Impulsive Cavalier",
-    "Valensi, Cunning Whisper",
-    "Zafinn, Mortal Wizard",
-]
-dungeons = [
-    "Highmount Village",
-    "Hellenburg",
-    "Cliffdrop City",
-    "Thorne Valley",
-    "Reachport",
-    "The Iron Peaks",
-    "Havington",
-    "Hogglebottom",
-    "Whittleberry",
-    "Bordertown",
-    "Abyssal Plane",
-    "Shan Tomb",
-    "Capital City",
-    "Stormshield",
-    "The Hungry Isles",
-    "Bakorio Province",
-    "Lariss",
-    "Filgan Forest",
-    "Tearburn Fields",
-    "Sunset Quarter",
-    "Aquira",
-    "Chundar Village",
-    "Belgor Mountain",
-    "Thatchwood",
-    "Alglen",
-    "Greatbridge",
-    "Dowlor",
-    "Eldrin",
-    "The Coils",
-    "Puffbag Town",
-    "Tunnels of Dreadmarsh",
-    "Crescent City",
-    "Meredrin",
-    "Hellenburg Clocktower",
-    "Bakorio City Ruins",
-    "Sphinx Fortress",
-]
+        bitmap = page.render(
+            scale=pngscale,
+            crop=(0, 0, 0, 292),
+        )
+        dungeon = bitmap.to_pil()
+        dungeon.save(f"combined/d{page_number+1}.png")
+    return(n_pages)
 
-# make all the pages
+def create_mixup_dungeons(characters,dungeons,n_pages):
+    """
+    make all the pages
+    """
+    for ii in range(n_pages):
+        for jj in range(n_pages):
+            im1 = Image.open(f"combined/c{ii+1}.png")
+            im2 = Image.open(f"combined/d{jj+1}.png")
 
-for ii in range(n_pages):
-    for jj in range(n_pages):
-        im1 = Image.open(f"combined/c{ii+1}.png")
-        im2 = Image.open(f"combined/d{jj+1}.png")
+            dungeonpage = merge(im1, im2,pngscale)
+            character = characters[ii]
+            dungeon = dungeons[jj]
+            dungeonpage.save(f"combined/The Adventures of {character} in {dungeon}.png")
 
-        dungeonpage = merge(im1, im2)
-        character = characters[ii]
-        dungeon = dungeons[jj]
-        dungeonpage.save(f"combined/The Adventures of {character} in {dungeon}.png")
+
+def main():
+    """
+    # you need to have libraries Pillow and Pypdfium2 installed. This is tested with Python 3.9.1 , pypdfium2 4.1.0, and pillow something or other
+
+    # importing pdf - put your version of the game here. Your folder should have this script and the game pdf and nothing else because I'm doing everything in place because I don't really understand file handling very well. If you have *both* the core set and the yearlong set then I'd recommend amalgamating the pdfs before you start or amending this script.
+
+    # if your pdf is called something different, change it below
+    """
+
+    # name all the characters and dungeons here for cutely named files.
+    # This is the core set and the first nine weeks of the yearlong set.
+    # You need to make sure the pages of the PDF are in the same order
+    #  as this or your names will all come out wrong.
+    # If you *only* have the year long set,
+    # delete the first six names from each list; they're the core items.
+    characters = [
+        "Zafinn, Wandering Wizard",
+        "Gloria, Avenging Warrior",
+        "Sygrid, Northern Hunter",
+        "Amador, Light Cleric",
+        "Mira, Haunted Ranger",
+        "Flynn, White Glove Knight",
+        "Amador, Steam Cleric",
+        "Sygrid, Vengeful Hunter",
+        "Gloria, Virtuous Warrior",
+        "Drok, Outcast Troll",
+        "Mira, Brush Ranger",
+        "Zafinn, Mischievous Wizard",
+        "Flynn, Red Glove Knight",
+        "Krete & Kreeg, Renowned Bards",
+        "Amador, Woodward Cleric",
+        "Zafinn, Elemental Wizard",
+        "Sygrid, Banished Hunter",
+        "Gloria, Daredevil Warrior",
+        "Valensi, Abyss Whisper",
+        "Mira, Lawless Ranger",
+        "Flynn, Blue Glove Knight",
+        "Drok, Adventurous Troll",
+        "Krete & Kreeg, Questing Bards",
+        "Zafinn, Brash Wizard",
+        "Hardos, Valiant Monk",
+        "Valensi, Spirit Whisper",
+        "Sygrid, Orphaned Hunter",
+        "Amador, Martial Cleric",
+        "Gloria, Night Warrior",
+        "Marian, Devious Spirit",
+        "Mira, Sky Ranger",
+        "Krete And Kreeg, Notorious Bards",
+        "Flynn, Black Glove",
+        "Finley,Impulsive Cavalier",
+        "Valensi, Cunning Whisper",
+        "Zafinn, Mortal Wizard",
+    ]
+    dungeons = [
+        "Highmount Village",
+        "Hellenburg",
+        "Cliffdrop City",
+        "Thorne Valley",
+        "Reachport",
+        "The Iron Peaks",
+        "Havington",
+        "Hogglebottom",
+        "Whittleberry",
+        "Bordertown",
+        "Abyssal Plane",
+        "Shan Tomb",
+        "Capital City",
+        "Stormshield",
+        "The Hungry Isles",
+        "Bakorio Province",
+        "Lariss",
+        "Filgan Forest",
+        "Tearburn Fields",
+        "Sunset Quarter",
+        "Aquira",
+        "Chundar Village",
+        "Belgor Mountain",
+        "Thatchwood",
+        "Alglen",
+        "Greatbridge",
+        "Dowlor",
+        "Eldrin",
+        "The Coils",
+        "Puffbag Town",
+        "Tunnels of Dreadmarsh",
+        "Crescent City",
+        "Meredrin",
+        "Hellenburg Clocktower",
+        "Bakorio City Ruins",
+        "Sphinx Fortress",
+    ]
+
+    n_pages = split_pdf(pngscale)
+    create_mixup_dungeons(characters,dungeons,n_pages)
+
+if __name__ == "__main__":
+    main()
